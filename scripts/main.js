512bc136955b27547dcd755756f695bae5bc1eda
Oleh <olehmoroz8@gmail.com>
2021-10-26 10:12:52 +0300

    ast

--- scripts/main.js
+++ scripts/main.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.main-carousel').slick({
         slidesToShow: 6.6,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 2500,
         speed: 1300,
         infinite: true,
